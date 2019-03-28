2d8d921fab72787377fed7db4918fd255335307b
jason-m-garner <jason.m.garner@gmail.com>
2019-03-28 17:22:21 -0500

    Updates

---
+++ precache-manifest.56dadf210e6f898c3536c66c1cd7d84b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "33330dbb6195ad74777e",
+    "url": "/ecomm-product-page-react/static/css/main.79dbfd53.chunk.css"
+  },
+  {
+    "revision": "33330dbb6195ad74777e",
+    "url": "/ecomm-product-page-react/static/js/main.33330dbb.chunk.js"
+  },
+  {
+    "revision": "b5d0dba42d14e624e404",
+    "url": "/ecomm-product-page-react/static/js/1.b5d0dba4.chunk.js"
+  },
+  {
+    "revision": "bb36a9eaca41a99de601",
+    "url": "/ecomm-product-page-react/static/js/runtime~main.bb36a9ea.js"
+  },
+  {
+    "revision": "2345276213ece8edf8278d4bb2e016cf",
+    "url": "/ecomm-product-page-react/index.html"
+  }
+];
